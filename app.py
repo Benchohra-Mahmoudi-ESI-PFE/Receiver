7b84aa4fcafb32ab395ec660c3149a8155ec735f
Sid Ali Mahmoudi <eng.sidali.mahmoudi@gmail.com>
2020-09-27 20:36:41 +0100

    Employee Id not autoincrement but custom

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ db = SQLAlchemy(app)
 
 class employees(db.Model):
     __tablename__ = 'employees'
-    id = db.Column(db.Integer, primary_key=True) #, default=db.session.query(func.public.generate_uid(5)).all())
+    id = db.Column(db.Text, primary_key=True, autoincrement=False) #, default=db.session.query(func.public.generate_uid(5)).all())
     employee_first_name = db.Column(db.String(50))
     employee_last_name = db.Column(db.String(50))
     employee_phone = db.Column(db.String(12),  unique=True)
@@ -57,7 +57,8 @@ class employees(db.Model):
     employee_role = db.Column(db.String(200))
     
 
-    def __init__(self, first_name, last_name, phone, proffession, banned, role):
+    def __init__(self, employee_id, first_name, last_name, phone, proffession, banned, role):
+        self.id = employee_id
         self.employee_first_name = first_name
         self.employee_last_name = last_name
         self.employee_phone = phone
@@ -81,7 +82,7 @@ class rooms(db.Model):
 
 class has_access(db.Model):
     __tablename__ = 'has_access'
-    employee_id = db.Column(db.Integer, db.ForeignKey(employees.id), primary_key=True)
+    employee_id = db.Column(db.Text, db.ForeignKey(employees.id), primary_key=True)
     room_id = db.Column(db.Integer, db.ForeignKey(rooms.id), primary_key=True)
     date_has_access = db.Column(db.Date)
     time_has_access = db.Column(db.Time)
@@ -101,7 +102,7 @@ class log_inscription(db.Model):
     id = db.Column(db.Integer, primary_key=True) #, default=db.session.query(func.public.generate_uid(5)).all())
     facial_biometric = db.Column(db.Text, unique=True) # path to preprocessed face data
     vocal_biometric = db.Column(db.Text, unique=True) # path to preprocessed voice data
-    employee_id = db.Column(db.Integer, db.ForeignKey(employees.id))
+    employee_id = db.Column(db.Text, db.ForeignKey(employees.id))
     date_inscription = db.Column(db.Date)
     time_inscription = db.Column(db.Time)
     inscription_description = db.Column(db.String(200))
@@ -120,7 +121,7 @@ class log_inscription(db.Model):
 
 class log_verification(db.Model):
     __tablename__ = 'log_verification'
-    employee_id = db.Column(db.Integer, db.ForeignKey(employees.id), primary_key=True)
+    employee_id = db.Column(db.Text, db.ForeignKey(employees.id), primary_key=True)
     room_id = db.Column(db.Integer, db.ForeignKey(rooms.id), primary_key=True)
     date_verification = db.Column(db.Date, primary_key=True)
     time_verification = db.Column(db.Time, primary_key=True)
@@ -139,7 +140,7 @@ class log_verification(db.Model):
 
 
 ################################################################################
-#db.drop_all()
+db.drop_all()
 db.create_all()
 db.session.commit()
 
